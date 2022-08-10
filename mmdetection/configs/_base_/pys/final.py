0105d31befc1bec094d1d26cd6ab7f71631e1c70
Riverskin1 <105196978+Riverskin1@users.noreply.github.com>
2022-08-10 11:47:35 +0900

    mmdetection
    mmdetection

---
+++ mmdetection/configs/_base_/pys/final.py
@@ -0,0 +1,6 @@
+_base_ = [
+    '/home/ubuntu/aq/mmdetection/configs/_base_/pys/model_FASTERRCNN.py',
+    '/home/ubuntu/aq/mmdetection/configs/_base_/pys/dataset.py',
+    '/home/ubuntu/aq/mmdetection/configs/_base_/pys/scheduler.py',
+    '/home/ubuntu/aq/mmdetection/configs/_base_/pys/default_runtime.py'
+]
